2f583fad8cb7d3c9967e36e00f2c11cb73fed1ec
Thiago kauai <html5ephp@gmail.com>
2019-03-19 18:37:09 -0300

    trabalhando com useEffect

--- src/App.js
+++ src/App.js
@@ -1,26 +1,43 @@
-import React, { useState } from 'react';
+import React, { useState,useEffect } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-export default function App(){
-  const [ repositories, setRepositories ] = useState([
-         {id:1,name:'teste1'},
-         {id:2,name:'teste2'},
-         {id:3,name:'teste3'}
-  ])
+export default function App() {
 
-  function handleAddRepository(){
-       setRepositories([...repositories,{id:Math.random(),name:"Teste"}])
+  const [ repositories, setRepositories ] = useState([])
+
+  useEffect(  async () => {
+    const repositories = await (await fetch('https://api.github.com/users/kauai/repos')).json()
+    setRepositories(repositories)
+  },[])
+
+  useEffect(() => {
+    document.title = repositories.filter(item => {
+       return item.favorite
+    }).length
+
+  },[repositories])
+
+  function handleFavorite(id) {
+     const newRepositories = repositories.map(repo => {
+        return repo.id == id ? {...repo, favorite:!repo.favorite } : repo
+     })
+     setRepositories(newRepositories)
   }
+
 
   return (
     <>
+    {console.log(repositories)}
       <ul>
         {repositories.map(item => {
-            return <li key={item.id}>{item.name}</li>
+            return <li key={item.id}>{item.name}
+               { item.favorite && <img style={{verticalAlign:"bottom"}} className="App-logo" width="40" src={logo}/>} 
+               <button onClick={(id) => handleFavorite(item.id)}>Favoritar</button>
+            </li>
         })}
       </ul>
-      <button onClick={handleAddRepository}>Clicar</button>
+      {/* <button onClick={handleAddRepository}>Clicar</button> */}
     </>
   )
 }
