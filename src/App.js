3425a3ee1cce5537cb414bc03acd61db1058e28e
Thiago kauai <html5ephp@gmail.com>
2019-03-19 19:32:12 -0300

    exemplo de hook com geolocalizaçao save commit

--- src/App.js
+++ src/App.js
@@ -1,43 +1,23 @@
-import React, { useState,useEffect } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from "react";
+import logo from "./logo.svg";
+import "./App.css";
 
 export default function App() {
-
-  const [ repositories, setRepositories ] = useState([])
-
-  useEffect(  async () => {
-    const repositories = await (await fetch('https://api.github.com/users/kauai/repos')).json()
-    setRepositories(repositories)
-  },[])
-
+  const [location, setLocation] = useState({});
   useEffect(() => {
-    document.title = repositories.filter(item => {
-       return item.favorite
-    }).length
+    navigator.geolocation.watchPosition(handlePositionReceived);
+  }, []);
 
-  },[repositories])
-
-  function handleFavorite(id) {
-     const newRepositories = repositories.map(repo => {
-        return repo.id == id ? {...repo, favorite:!repo.favorite } : repo
-     })
-     setRepositories(newRepositories)
+  function handlePositionReceived({ coords }) {
+    const { latitude, longitude } = coords;
+    setLocation({ latitude ,longitude })
   }
-
 
   return (
     <>
-    {console.log(repositories)}
-      <ul>
-        {repositories.map(item => {
-            return <li key={item.id}>{item.name}
-               { item.favorite && <img style={{verticalAlign:"bottom"}} className="App-logo" width="40" src={logo}/>} 
-               <button onClick={(id) => handleFavorite(item.id)}>Favoritar</button>
-            </li>
-        })}
-      </ul>
-      {/* <button onClick={handleAddRepository}>Clicar</button> */}
+      <span>{location.latitude}</span>
+      <br />
+      <span>{location.longitude}</span>
     </>
-  )
+  );
 }
